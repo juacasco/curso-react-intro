939daa2d1c38f65aa42a3e4dfade68c4a35ed380
Juan Pablo Carrasco <juacasco@gmail.com>
2023-05-29 20:26:16 -0500

    Initial changes ---

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
     </div>
   );
 }
 
+function TodoCounter() {
+  return (
+    <h1>Has comnpletado 2 de 3 TODOs</h1>
+  )
+}
+
+function TodoSearch() {
+  return (
+    <h1>Has comnpletado 2 de 3 TODOs</h1>
+  )
+}
+
+function TodoItem() {
+  return(
+    <li>
+      <span>V</span>
+      <p>Elemento Todo</p>
+      <span>X</span>
+    </li>
+  )
+  
+}
 export default App;
